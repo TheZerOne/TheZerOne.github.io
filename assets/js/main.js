ff09916e681b6e3d90c25ab08a90d24786c4e71e
Sathish Ravichandran <ravichandran.sathish@gmail.com>
2021-06-09 22:42:39 -0400

    layout update

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 2000,
+    duration: 1000,
     reset: true
 });
 
@@ -41,14 +41,14 @@ sr.reveal('.home__social-icon',{ interval: 200});
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__subtitle',{delay: 200}); 
 sr.reveal('.about__text',{delay: 400}); 
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+// sr.reveal('.skills__subtitle',{}); 
+// sr.reveal('.skills__text',{}); 
+// sr.reveal('.skills__data',{interval: 200}); 
+// sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 200}); 
