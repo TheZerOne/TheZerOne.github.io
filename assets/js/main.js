2b3e852d3a9c311ffc738ac03f8d0f9a5c58d864
Sathish Ravichandran <ravichandran.sathish@gmail.com>
2021-06-23 21:32:22 -0400

    buttonupdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,8 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
+sr.reveal('.main-btn',{delay: 400}); 
+sr.reveal('.main-btn-save',{delay: 400}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
