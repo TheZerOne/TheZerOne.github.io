944d42739ec2a5881223378228a8bfff60f5237c
TheZerOne <ravichandran.sathish@gmail.com>
2024-07-23 14:47:05 -0700

    MV BC update

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,9 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
+sr.reveal('.about section',{delay: 200}); 
 sr.reveal('.main-btn',{delay: 400}); 
+sr.reveal('.mains-btn',{delay: 400});
 sr.reveal('.main-btn-save',{delay: 400}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
@@ -57,7 +59,4 @@ sr.reveal('.work__img',{interval: 200});
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
-
-
-
 
